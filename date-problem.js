e4a5d21721695dc15872c64090ba53bd30ba25c6
mbrie041 <mbrie041@gmail.com>
2022-01-15 13:03:00 -0500

    Create function to find an array of ranges

--- date-problem.js
+++ date-problem.js
@@ -1,5 +1,4 @@
 const gapAnalysis = function (list, min, max) {
-  const gapArray = [];
   const listValues = list.map(([minVal, maxVal]) => rangeMaker(minVal, maxVal));
   const listedRange = [];
   const gapArrayValues = rangeMaker(min, max);
@@ -12,14 +11,12 @@ const gapAnalysis = function (list, min, max) {
       }
     });
   });
-  console.log(listedRange);
+  // console.log(listedRange);
 
   //check if gapArrayValues are included in listedRange
-  gapArrayValues.forEach((element, index) => {
-    if (!listedRange.includes(element)) {
-      gapArray.push(element);
-    }
-  });
+  const gapArray = gapArrayValues.filter(
+    (element) => !listedRange.includes(element)
+  );
 
   //turn the new array into an array of nested arrays
 
@@ -35,16 +32,45 @@ const rangeMaker = function (min, max) {
   }
   return output;
 };
+//given an array of increasing values
+//construct an array of ranges
+const rangeFinder = function (values) {
+  const sortedArray = values.slice().sort(function (a, b) {
+    return a - b;
+  });
+  let startingValue = sortedArray[0];
+  let endingValue = sortedArray[values.length - 1];
+  const returnedArray = [];
+  let previousElement;
+
+  sortedArray.forEach((element) => {
+    if (element > previousElement + 1) {
+      returnedArray.push([startingValue, previousElement]);
+      startingValue = element;
+      previousElement = element;
+    } else if (element === endingValue) {
+      returnedArray.push([startingValue, element]);
+    } else {
+      previousElement = element;
+    }
+  });
+
+  if (returnedArray.length === 1) {
+    const newReturnedArray = returnedArray[0];
+    return newReturnedArray;
+  } else return returnedArray;
+};
 
 module.exports = {
   gapAnalysis,
   rangeMaker,
+  rangeFinder,
 };
 const list = [
   [3, 5],
   [6, 8],
 ];
 const min = 1;
-const max = 10;
+const max = 20;
 gapAnalysis(list, min, max);
 // rangeMaker(1, 3);
--- test/tests.js
+++ test/tests.js
@@ -2,6 +2,7 @@ const assert = require("chai").assert;
 const expect = require("chai").expect;
 const gapAnalysis = require("../date-problem").gapAnalysis;
 const rangeMaker = require("../date-problem").rangeMaker;
+const rangeFinder = require("../date-problem").rangeFinder;
 
 // const list = [
 //   [3, 5],
@@ -21,60 +22,93 @@ const rangeMaker = require("../date-problem").rangeMaker;
 // expected result = gaps [day 1, day 6, day 10] = [[1,2], [6,7], [9,10]]?
 
 describe("date problem", function () {
-  it("should return an array", function () {
-    const list = [
-      [3, 5],
-      [6, 8],
-    ];
-    const min = 1;
-    const max = 10;
-    const result = gapAnalysis(list, min, max);
+  // it("should return an array", function () {
+  //   const list = [
+  //     [3, 5],
+  //     [6, 8],
+  //   ];
+  //   const min = 1;
+  //   const max = 10;
+  //   const result = gapAnalysis(list, min, max);
+  //   assert.typeOf(result, "array");
+  // });
+
+  // it("should return an array of all numbers between min and max", function () {
+  //   const min = 1;
+  //   const max = 3;
+  //   const expectedResult = [1, 2, 3];
+  //   const result = rangeMaker(min, max);
+  //   expect(expectedResult).to.eql(result);
+  // });
+
+  // it("should return an array of a single value when give that as a range", function () {
+  //   const min = 1;
+  //   const max = 1;
+  //   const expectedResult = [1];
+  //   const result = rangeMaker(min, max);
+  //   expect(expectedResult).to.eql(result);
+  // });
+
+  // it("should return an array with the gaps from the given list", function () {
+  //   const list = [
+  //     [1, 7],
+  //     [2, 5],
+  //     [3, 8],
+  //   ];
+
+  //   const min = 1;
+  //   const max = 10;
+
+  //   const expectedResult = [[9, 10]];
+  //   const result = gapAnalysis(list, min, max);
+  //   expect(expectedResult).to.eql(result);
+  // });
+
+  // it("should return an array with the gaps from the given list", function () {
+  //   const list = [
+  //     [3, 5],
+  //     [6, 8],
+  //   ];
+  //   const min = 1;
+  //   const max = 10;
+  //   const expectedResult = [
+  //     [1, 2],
+  //     [9, 10],
+  //   ];
+  //   const result = gapAnalysis(list, min, max);
+  //   expect(expectedResult).to.eql(result);
+  // });
+
+  it("rangeFinder should return an array", function () {
+    const values = [1, 2, 3, 4, 5, 6];
+    const result = rangeFinder(values);
     assert.typeOf(result, "array");
   });
 
-  it("should return an array of all numbers between min and max", function () {
-    const min = 1;
-    const max = 3;
-    const expectedResult = [1, 2, 3];
-    const result = rangeMaker(min, max);
+  it("rangeFinder should return an array of a range given an array of increasing values", function () {
+    const values = [1, 2, 3, 4, 5, 6];
+    const result = rangeFinder(values);
+    const expectedResult = [1, 6];
     expect(expectedResult).to.eql(result);
   });
 
-  it("should return an array of a single value when give that as a range", function () {
-    const min = 1;
-    const max = 1;
-    const expectedResult = [1];
-    const result = rangeMaker(min, max);
-    expect(expectedResult).to.eql(result);
-  });
-
-  it("should return an array with the gaps from the given list", function () {
-    const list = [
-      [1, 7],
-      [2, 5],
-      [3, 8],
-    ];
-
-    const min = 1;
-    const max = 10;
-
-    const expectedResult = [[9, 10]];
-    const result = gapAnalysis(list, min, max);
-    expect(expectedResult).to.eql(result);
-  });
-
-  it("should return an array with the gaps from the given list", function () {
-    const list = [
-      [3, 5],
-      [6, 8],
-    ];
-    const min = 1;
-    const max = 10;
+  it("rangeFinder should return an array of multiple ranges given an array of increasing values and a gap", function () {
+    const values = [1, 2, 4, 5, 6];
+    const result = rangeFinder(values);
     const expectedResult = [
       [1, 2],
-      [9, 10],
+      [4, 6],
     ];
-    const result = gapAnalysis(list, min, max);
+    expect(expectedResult).to.eql(result);
+  });
+
+  it("rangeFinder should return an array of multiple ranges given an array of increasing values and a gap", function () {
+    const values = [1, 2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    const result = rangeFinder(values);
+    const expectedResult = [
+      [1, 2],
+      [9, 20],
+    ];
     expect(expectedResult).to.eql(result);
   });
 });
